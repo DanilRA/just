9314b12878beb76b9343587b5c8113fff30e642e
Danil <danilraaa07@gmail.com>
2024-07-17 15:04:26 +0530

    Done successfully

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from typing import Union
 from typing import List, Tuple
 from fastapi import FastAPI, Request, Form, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,StreamingResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from io import BytesIO
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = FastAPI()
 
+app.mount("/static",StaticFiles(directory="static"), name = "static")
 templates = Jinja2Templates(directory="templates")
 
 
@@ -19,20 +24,43 @@ async def process_tup(request: Request):
     form_data = await request.form()
     tuple_str = form_data.get("val")
     tuple_list = tuple_str.split(";")
-    print(tuple_list)
-    org_list = []
+    x = []
+    y = []
     for tup in tuple_list:
         tup = tup.split("(") 
         s = tup[1].split(")")
         a = s[0].split(",")
-        print(a)
-        org_list.append((int(a[0]),int(a[1])))
-    print(org_list)
-        
-
-
-
+        x.append(int(a[0]))
+        y.append(int(a[1]))
+    print(x)
+    print(y)
     
+    global area
+    area = np.trapz(y,x)
+    new_area = str(area)
+    
+
+    plt.plot(x, y)
+    plt.xlabel("Time(s)")
+    plt.ylabel("Velocity(m/s)")
+    plt.fill_between(x,y,color = "skyblue",alpha = 0.4)
+    plt.plot(x,y, color = "slateblue",alpha = 0.6)
+
+
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    plt.close()
+
+    global image_data
+    image_data = buf
+
+    return templates.TemplateResponse("content.html",{"request" : request, "area":new_area})
+
+@app.get("/process")
+async def image():
+    return StreamingResponse(image_data, media_type= "image/png")
+
 
 
 
